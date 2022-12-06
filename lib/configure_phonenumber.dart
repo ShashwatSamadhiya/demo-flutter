9a6d6132646e7d3edf4ca238572bf7feea52a0ee
ShashwatSamadhiya <sshashwat934@gmail.com>
2022-12-06 16:30:54 +0530

    sixth commit

--- lib/configure_phonenumber.dart
+++ lib/configure_phonenumber.dart
@@ -23,22 +23,27 @@ class PhoneNumberConfigureScreen extends StatefulWidget {
 class _PhoneNumberConfigureScreenState
     extends State<PhoneNumberConfigureScreen> {
   bool? isStillDefault;
-  String? isTag;
+  String isTag = "..";
   Color? isColor;
 
   Widget cservice(String text, Icon ic) {
     return Container(
         padding: const EdgeInsets.symmetric(
-          horizontal: 10,
+          horizontal: 5,
           vertical: 5,
         ),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
-          color: Colors.grey[200],
+          color: Color.fromARGB(255, 246, 245, 246),
         ),
         child: Wrap(children: [
           ic,
-          Text(text),
+          Text(
+            text,
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
         ]));
   }
 
@@ -78,45 +83,83 @@ class _PhoneNumberConfigureScreenState
   Widget _getCard(BuildContext context) {
     return Container(
       padding: const EdgeInsets.only(bottom: 15.0),
-      margin: EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 0),
+      margin: EdgeInsets.only(left: 20, top: 15, right: 20, bottom: 0),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(20),
       ),
       child: Column(
         children: [
-          const ListTile(
-            leading: FlutterLogo(),
-            title: Text(
-              "+1 111 111 1111",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            subtitle: Text("India"),
+          ListTile(
+            // leading: FlutterLogo(),
+            leading: (CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://img.freepik.com/premium-photo/india-flag-background-design-independence-day-other-celebration_46250-2225.jpg?w=2000"),
+            )),
+            title: Container(
+                margin: EdgeInsets.only(left: 5),
+                child: Text(
+                  "+1 111 111 1111",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+            subtitle: Container(
+                margin: EdgeInsets.only(left: 5), child: Text("India")),
           ),
           Container(
-            color: Colors.grey,
-            child: const ListTile(
+            color: Color.fromARGB(255, 246, 245, 246),
+            child: ListTile(
               title: Text(
-                "+1 111 111 1111",
+                isTag,
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              subtitle: Text("India"),
-              trailing: FlutterLogo(),
+              // subtitle: Text("Renewable on " + "ndf"),
+              subtitle: Container(
+                  margin: EdgeInsets.only(
+                    top: 5,
+                  ),
+                  child: RichText(
+                    text: TextSpan(
+                        text: "Renewable on",
+                        style: TextStyle(
+                          fontSize: 11,
+                          color: Colors.black,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: " 22 Aug, 2022",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ))
+                        ]),
+                  )),
+              trailing: Container(
+                  margin: EdgeInsets.only(right: 10),
+                  child: isTag == ".."
+                      ? null
+                      : Text(
+                          isTag[0],
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: isColor),
+                        )),
             ),
           ),
           ListTile(
-            title: const Text(
-              "Services",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            title: Container(
+                margin: EdgeInsets.only(top: 10),
+                child: const Text(
+                  "Services",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
             subtitle: Container(
-                margin: EdgeInsets.only(top: 10, left: 0, right: 0, bottom: 0),
+                margin: EdgeInsets.only(top: 10),
                 child: Row(
                   children: [
                     Expanded(
@@ -133,16 +176,13 @@ class _PhoneNumberConfigureScreenState
   Widget tag(String text) {
     return Container(
       padding: const EdgeInsets.symmetric(
-        // horizontal: 5,
-        vertical: 10,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.grey[850],
+        horizontal: 0,
+        vertical: 0,
       ),
       child: TextButton(
           onPressed: () {
             setState(() {
+              isTag = text;
               isColor = widget.tags[text];
             });
           },
@@ -150,16 +190,28 @@ class _PhoneNumberConfigureScreenState
             text,
             textAlign: TextAlign.center,
             style: TextStyle(
-              color: Colors.white,
+              color: isTag == text ? Colors.black : Colors.white,
             ),
           )),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        border: Border.all(color: Colors.grey, width: 1),
+        color: isTag == text ? Colors.white : Colors.grey[850],
+      ),
     );
   }
 
   Widget col(Color clr) {
     return Container(
-        margin: EdgeInsets.only(top: 10, left: 0, right: 0, bottom: 0),
+        padding: const EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 0),
+        //margin: EdgeInsets.only(top: 10, left: 0, right: 0, bottom: 0),
         child: TextButton(
+            style: TextButton.styleFrom(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 0,
+                vertical: 0,
+              ),
+            ),
             onPressed: () {
               setState(() {
                 isColor = clr;
@@ -189,9 +241,10 @@ class _PhoneNumberConfigureScreenState
       ), // Options that are getting passed to the ListView.builder() function
       children: [
         //tags.map((t) => tag(t)).toList(),
+        tag("Bussiness & work"),
         tag("Personal"),
-        tag("family"),
-        tag("stranger"),
+        tag("Family"),
+        tag("Stranger"),
         tag("Transaction and otp"),
         tag("Add new+")
       ], // The list of widgets in the list
@@ -311,12 +364,12 @@ class _PhoneNumberConfigureScreenState
 
   Widget getColor(BuildContext context) {
     return ResponsiveGridList(
-      horizontalGridSpacing: 16, // Horizontal space between grid items
-      verticalGridSpacing: 16, // Vertical space between grid items
-      horizontalGridMargin: 20, // Horizontal space around the grid
+      horizontalGridSpacing: 10, // Horizontal space between grid items
+      verticalGridSpacing: 10, // Vertical space between grid items
+      horizontalGridMargin: 1, // Horizontal space around the grid
       verticalGridMargin: 20, // Vertical space around the grid
       minItemWidth:
-          80, // The minimum item width (can be smaller, if the layout constraints are smaller)
+          30, // The minimum item width (can be smaller, if the layout constraints are smaller)
       minItemsPerRow:
           1, // The minimum items to show in a single row. Takes precedence over minItemWidth
       maxItemsPerRow:
@@ -352,7 +405,7 @@ class _PhoneNumberConfigureScreenState
           title: Padding(
               padding: const EdgeInsets.only(bottom: 0),
               child: Text(
-                "Color",
+                "Colour",
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
@@ -382,19 +435,42 @@ class _PhoneNumberConfigureScreenState
 
   @override
   Widget build(BuildContext context) {
-    print('i m building');
+    //print('i m building');
     isStillDefault = widget.isDefaultCallerId;
 
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 40, 38, 38),
+        title: const Text('ASSIGN A TAG',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            )),
+        actions: [
+          TextButton(
+              onPressed: isTag == ".." ? () => {} : () => {print("hello")},
+              child: Text(
+                "Done",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: isTag == ".." ? Colors.grey[400] : Colors.white,
+                  fontWeight: isTag == ".." ? null : FontWeight.bold,
+                  fontSize: 16,
+                ),
+              )),
+        ],
+      ),
       body: SingleChildScrollView(
           child: Container(
+        constraints: BoxConstraints(minWidth: 100, maxWidth: 450),
         color: Colors.black,
         child: Column(
           children: [
             _getCard(context),
             gt(context),
             gc(context),
-            getButton(context),
+            //getButton(context),
           ],
         ),
       )),
